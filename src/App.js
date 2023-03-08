5c2916ef4507cb050322a1ac1b28a0b554287e7f
hana-nazer <hanapp9966@gmail.com>
2023-03-09 00:27:47 +0530

    showing error pages with error element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/Error";
 import Home from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/RootLayout";
@@ -8,6 +9,7 @@ function App() {
     {
       path: "/",
       element: <RootLayout />,
+      errorElement:<ErrorPage/>,
       children: [
         { path: "/", element: <Home /> },
         { path: "/products", element: <ProductsPage /> },
