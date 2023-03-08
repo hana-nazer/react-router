0312976ee55fd00c8ad6d8c3e1cfba90d01ba638
hana-nazer <hanapp9966@gmail.com>
2023-03-08 20:37:24 +0530

    defining routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductsPage from './pages/Products'
+
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([{ path: "/", element: <Home /> },
+{path:'/products',element:<ProductsPage/>}]);
+  return <div>
+    <RouterProvider router={router}/>
+  </div>;
 }
 
 export default App;
