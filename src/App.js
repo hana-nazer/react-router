0a28452b358a468d6e52b24f39bf6bece8a9adc8
hana-nazer <hanapp9966@gmail.com>
2023-03-09 00:16:48 +0530

    layouts and nesting routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
-import ProductsPage from './pages/Products'
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/RootLayout";
 
 function App() {
-  const router = createBrowserRouter([{ path: "/", element: <Home /> },
-{path:'/products',element:<ProductsPage/>}]);
-  return <div>
-    <RouterProvider router={router}/>
-  </div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/products", element: <ProductsPage /> },
+      ],
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
